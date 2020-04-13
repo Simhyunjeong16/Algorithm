9810da44c9daa7847e7b3d9fa0a468d6a4157085
Simhyunjeong16 <shj2369@gmail.com>
2020-04-13 21:56:37 +0900

    11053번_가장 긴 증가하는 부분 수열 [DP]

---
+++ dp/11053.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int a[1001];
+int d[1001];
+
+int main(){
+    int num;
+    cin >> num; //수열의 크기
+
+
+    for(int i=0; i<num; i++){
+        cin >> a[i];
+    }
+
+
+    int result = 0;
+    for(int i=0; i<num; i++){
+        d[i] = 1;
+
+        for(int j=0; j<i; j++){
+            if(a[j] < a[i]){
+                d[i] = max(d[i], d[j]+1);//점화식
+            }
+        }
+        result = max(result, d[i]);
+    }
+
+
+    cout << result;
+
+    return 0;
+}
