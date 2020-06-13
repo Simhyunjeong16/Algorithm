95e8a731c205e8bce3bbbb23ac6180f178ee9a3f
Simhyunjeong16 <shj2369@gmail.com>
2020-06-14 00:05:50 +0900

    1929번_소수 구하기 [Math, 에라토스테네스의 체]

---
+++ math/1929.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int m, n;
+int arr[1000000];
+
+int main(){
+ 
+    cin >> m >> n;
+
+    for(int i = 2; i <= n; i++){
+        arr[i] = i;
+    }
+
+ 
+    for(int i = 2; i <= n; i++){
+        if(arr[i] == 0) continue;
+
+        for(int j = i+i; j <= n; j += i){
+            arr[j] = 0;
+        }
+    }
+
+
+    for(int i = m; i <= n; i++){
+        if(arr[i] != 0) {
+            cout << arr[i] << "\n";
+        }
+    }
+
+
+    return 0;
+}
