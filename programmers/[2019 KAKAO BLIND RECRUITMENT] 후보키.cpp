183f9eb61e8333cbf45a21cd55483e58585990f2
Simhyunjeong16 <shj2369@gmail.com>
2020-08-14 00:04:39 +0900

    [2019 KAKAO BLIND RECRUITMENT] 후보키

--- programmers/[2019 KAKAO BLIND RECRUITMENT] 후보키.cpp
+++ programmers/[2019 KAKAO BLIND RECRUITMENT] 후보키.cpp
@@ -1,71 +1,78 @@
 #include <string>
 #include <vector>
 #include <algorithm>
-#include <stdlib.h>
+#include <map>
 #include <iostream>
-#include <math.h>
 
 using namespace std;
 
-struct fname{
-    string head;
-    int num;
-    int index;
-};
+vector<string> com;
+int column_cnt = 0;
 
-bool comp(fname x, fname y) {
-    std::transform(x.head.begin(), x.head.end(), x.head.begin(), ::tolower);//string 소문자로 변경
-    std::transform(y.head.begin(), y.head.end(), y.head.begin(), ::tolower);
-    
-    if(x.head == y.head)//헤더가 같다면
-        return x.num < y.num;//오름차순
-    return x.head < y.head;//오름차순
+bool cmp(string& a, string& b){//길이 순으로 오름차순 -> 숫자 오름차순
+    if(a.length() != b.length()){
+        return a.length() < b.length();
+    }
+    return a < b;
 }
 
-vector<string> solution(vector<string> files) {
-    vector<string> answer;
-    vector<fname> name;
-    fname f;
-    
-    for(int i=0; i<files.size(); i++){
-        
-        for(int j=0; j<files[i].size(); j++){
-            if(files[i][j] >= '0' && files[i][j] <= '9'){
-                f.head = files[i].substr(0, j);//0번째부터 j의 길이만큼 자르기
-                
-                
-                string str = "";
-                vector<int> nums;
-                for(int k=j; k<files[i].size(); k++){
-                    if( files[i][k] >= '0' && files[i][k] <= '9') {
-                        nums.push_back(files[i][k]-'0');
-                    }
-                    else break;
-                    
-                }
-                
-                int result = 0;
-                for(int k=nums.size()-1; k >= 0; k--){
-                    result += nums[k] * pow(10, nums.size()-1 -k);
-                }
-    
-                f.num = result;
-                f.index = i;
-                // cout << f.head << "  " << result << endl;
-                name.push_back(f);
-                break;
-            }
-            
-        }
-        
+void Combination(int k = 0, string t= ""){//모든 조합을 만드는 함수
+    for(int i = k; i < column_cnt; i++){
+        t += to_string(i);
+        com.push_back(t);
+        Combination(i+1, t);
+        t = t.substr(0, t.length()-1);//뒤에 하나 없애기
     }
-    
-    stable_sort(name.begin(), name.end(), comp);//정렬후 동일값의 원소 순서를 보장
-    
-    
-    for(int i=0; i<name.size(); i++){
-        answer.push_back(files[name[i].index]);
-    }
+}
 
+//조합에 따른 후보키 판단 함수
+bool subset(vector<vector<string>>& relation, string c){
+    map<string, int> chk;
+    
+    for(int i = 0; i < relation.size(); i++) {
+        
+        //후보키 조합에 따른 string 합치기
+        string tmp = "";
+        for(int j = 0; j < c.length(); j++){
+            int n = c[j] - '0';
+            tmp += relation[i][n] + " ";
+        }
+
+        if(chk[tmp]) return false;//이미 있다면 후보키 불가
+
+        chk[tmp]++;
+    }
+    return true;//끝까지 중복이 없다면, 최소성과 유일성 만족 true
+}
+
+int solution(vector<vector<string>> relation) {
+    int answer = 0;
+    column_cnt = relation[0].size();
+    
+    Combination();//가능한 조합수 가져오기
+
+    sort(com.begin(), com.end(), cmp);//가능한 조합수 정렬(낮은 조합부터) -> 오름차순
+    // for(int i = 0; i < com.size(); i++) cout << com[i] << " ";
+    
+    //조합수만큼 반복
+    for(int i = 0; i < com.size(); i++){
+        if(subset(relation, com[i])){//후보키라면
+            string str = com[i];
+            
+            for(int j = com.size()-1; j >= i; j--){//후보키와 겹치는 조합 없애기
+                bool flag = true;
+                for(auto s : str){
+                    if(com[j].find(s) == string::npos){//찾는 문자열이 없는 경우, string::npos를 반환한다.
+                        flag = false;
+                        break;
+                    }
+                }
+                
+                if(flag) com.erase(com.begin() + j);
+            }
+            i--;
+            answer++;
+        }
+    }
     return answer;
 }
