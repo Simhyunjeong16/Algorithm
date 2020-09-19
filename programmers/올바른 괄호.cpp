c9dc4f53cf55c72539fea497c8ec21eb5d815704
Simhyunjeong16 <shj2369@gmail.com>
2020-09-20 00:22:59 +0900

    올바른 괄호

---
+++ programmers/올바른 괄호.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s){
+    bool answer = true;
+    stack<char> st;
+    
+    for(int i = 0; i < s.length(); i++){
+        if(!st.empty() && st.top() == '(' && s[i] == ')') st.pop();
+        else st.push(s[i]);    
+    }
+    
+    if(!st.empty()) answer = false;
+
+    return answer;
+}
