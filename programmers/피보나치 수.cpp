651bd6585d03f223f3377dbede70d08dba87d2eb
Simhyunjeong16 <shj2369@gmail.com>
2020-09-23 00:42:06 +0900

    피보나치 수

---
+++ programmers/피보나치 수.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int n) {
+    vector<int> v = {0, 1};
+    
+    for(int i = 2; i <= n; i++){   
+        v.push_back((v[i-1] + v[i-2]) % 1234567);
+    }
+    
+    return v[n];
+}
