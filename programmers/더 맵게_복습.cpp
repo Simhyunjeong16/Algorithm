550fb23f1ab5e70e52012d10ddd222de11785458
Simhyunjeong16 <shj2369@gmail.com>
2020-11-05 00:06:38 +0900

    더 맵게 [Priority_queue]_복습

---
+++ programmers/더 맵게_복습.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int solution(vector<int> scoville, int K) {
+    int answer = 0;
+    priority_queue<int, vector<int>, greater<int> > pq(scoville.begin(), scoville.end());
+    
+    while(pq.top() < K && pq.size() >= 2){
+        int x = pq.top();
+        pq.pop();
+        
+        int y = pq.top();
+        pq.pop();
+        
+        pq.push(x + (y * 2));
+        answer++;
+    }
+    
+    if(pq.size() == 1 && pq.top() < K) answer = -1;
+    
+    return answer;
+}
