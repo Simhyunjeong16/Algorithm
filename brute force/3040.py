2f7e4f0e0f0a976eca6206257b5bf435e7644daf
Simhyunjeong16 <shj2369@gmail.com>
2020-11-14 00:20:52 +0900

    3040번_백설공주와 일곱난쟁이 [Brute force, combination]

---
+++ brute force/3040.py
@@ -0,0 +1,9 @@
+from itertools import combinations
+
+n = [int(input()) for _ in range(9)]
+
+result = list(combinations(n, 7))
+
+for i in result:
+    if sum(i) == 100:
+        for j in i: print(j)
