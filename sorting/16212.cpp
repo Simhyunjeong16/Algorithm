8ff6f69fa1fdb56c6626efbe99e439ddbdc68f85
Simhyunjeong16 <shj2369@gmail.com>
2021-11-18 22:12:22 +0900

    16212번_정열적인 정렬 [Sorting]

---
+++ sorting/16212.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(){
+    int a[500001];
+    int n;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+
+    sort(a, a+n);
+
+    for(int i = 0; i < n; i++){
+        printf("%d ", a[i]);
+    }
+
+    return 0;
+}
