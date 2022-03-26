eca8539dcb89d57dd6890324fab47438c1c4b504
Simhyunjeong16 <shj2369@gmail.com>
2022-03-27 00:10:15 +0900

    The PADS_SUBSTR_||(문자열이어붙이기)

---
+++ hackerrank/sql/advanced select/The PADS.sql
@@ -0,0 +1,8 @@
+select name || '(' || substr(occupation,1,1) || ')'
+from OCCUPATIONS
+order by name;
+
+select 'There are a total of ' || count(occupation) || ' ' || lower(occupation) || 's.'
+from OCCUPATIONS
+group by occupation
+order by count(occupation), occupation;
